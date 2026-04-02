a41d69591014fba3a782c23622aaadda57c910fa
pranjaligodbole-dev <pranjaligodbole23@gmail.com>
2026-04-02 13:21:26 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -113,7 +113,7 @@ if(gameState === "end"){
   // 👜 PRODUCTS (moved UP into pouch)
   let pouchY = height * 0.34;
   let spacing = 95;
-  let startX = width/2 - spacing - 20;
+  let startX = width/2 - spacing - 15;
 
   for(let i=0;i<3;i++){
     let label = slots[i];
@@ -130,7 +130,7 @@ if(gameState === "end"){
   fill(0);
   textSize(20);
   textAlign(CENTER, CENTER);
-  text(timer + "s", width/2, height * 0.62);
+  text(timer + "s", width/2, height * 0.59);
 
   return;
 }
